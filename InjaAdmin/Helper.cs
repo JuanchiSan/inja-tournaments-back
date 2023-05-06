cc34ac2130468fc22e7a7bb1da7c23f45824c2da
Gustavo A. Correa Reina <guadcore@gmail.com>
2023-05-05 22:06:14 -0300

    New version

---
+++ InjaAdmin/Helper.cs
@@ -0,0 +1,21 @@
+﻿namespace InjaAdmin;
+
+public static class Helper
+{
+	private static InjaData.Models.dbContext? _db;
+
+	public static InjaData.Models.dbContext DB
+	{
+		get
+		{
+			_db ??= new InjaData.Models.dbContext();
+			return _db;
+		}
+	}
+
+	public static void ResetContext()
+	{
+		_db = new InjaData.Models.dbContext();
+		GC.Collect();
+	}
+}
