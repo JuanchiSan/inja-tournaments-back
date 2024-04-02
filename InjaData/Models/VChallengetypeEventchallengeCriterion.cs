eebbfc2fca1ba7395327b4dca5b81381387dabfa
Gustavo A. Correa Reina <guadcore@gmail.com>
2024-04-02 09:00:35 -0300

    New cups

---
+++ InjaData/Models/VChallengetypeEventchallengeCriterion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InjaData.Models;
+
+public partial class VChallengetypeEventchallengeCriterion
+{
+    public int? Challengetypeid { get; set; }
+
+    public int? Eventchallengeid { get; set; }
+
+    public int? Challengejudgementid { get; set; }
+
+    public string? Challengetypename { get; set; }
+
+    public string? Eventchallengename { get; set; }
+
+    public int? Criteriaid { get; set; }
+
+    public string? Criterianame { get; set; }
+
+    public string? Criterianamees { get; set; }
+
+    public decimal? Maxscore { get; set; }
+
+    public decimal? Slotstep { get; set; }
+
+    public int? Slotcant { get; set; }
+
+    public int? Hands { get; set; }
+
+    public short? Rounds { get; set; }
+}
---
+++ InjaData/Models/VCupNailArtParticipantsOk.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InjaData.Models;
+
+public partial class VCupNailArtParticipantsOk
+{
+    public int? Contenderid { get; set; }
+
+    public int? Divisionid { get; set; }
+
+    public int? Eventid { get; set; }
+}
